11c70584bef6dc992bea465ce86d32e8b5462a98
Chamil Fonseka <fonseka.chamil@gmail.com>
2024-10-16 21:55:46 +0530

    added the chat-client project

---
+++ chat-client/src/test/java/dev/chafon/spring/ai/chatclient/ChatClientApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.chafon.spring.ai.chatclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
