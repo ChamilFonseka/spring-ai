70fd1a8aed54dd7dd2e34f6015f6f21e0baa0e8c
Chamil Fonseka <fonseka.chamil@gmail.com>
2024-10-18 21:05:55 +0530

    added the basic chatClient configuration

--- chat-client/src/main/java/dev/chafon/spring/ai/chatclient/ChatController.java
+++ chat-client/src/main/java/dev/chafon/spring/ai/chatclient/ChatController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
 
 @RestController
 @RequestMapping("/chat")
@@ -12,8 +13,8 @@ public class ChatController {
 
     private final ChatClient chatClient;
 
-    public ChatController(ChatClient.Builder chatClientBuilder) {
-        this.chatClient = chatClientBuilder.build();
+    public ChatController(ChatClient chatClient) {
+        this.chatClient = chatClient;
     }
 
     @GetMapping
