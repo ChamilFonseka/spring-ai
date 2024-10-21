62cd4a724ade47c18ff9dfd8548e14b42040cc2e
Chamil Fonseka <fonseka.chamil@gmail.com>
2024-10-21 15:00:56 +0530

    added the in-memory chat history configuration

--- chat-client/src/main/java/dev/chafon/spring/ai/chatclient/ChatController.java
+++ chat-client/src/main/java/dev/chafon/spring/ai/chatclient/ChatController.java
@@ -5,7 +5,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import reactor.core.publisher.Flux;
+
+import static org.springframework.ai.chat.client.advisor.AbstractChatMemoryAdvisor.CHAT_MEMORY_CONVERSATION_ID_KEY;
+import static org.springframework.ai.chat.client.advisor.AbstractChatMemoryAdvisor.CHAT_MEMORY_RETRIEVE_SIZE_KEY;
 
 @RestController
 @RequestMapping("/chat")
@@ -18,8 +20,12 @@ public class ChatController {
     }
 
     @GetMapping
-    String chat(@RequestParam(value = "message", defaultValue = "Hello") String message) {
+    String chat(@RequestParam(value = "message", defaultValue = "Hello") String message,
+                @RequestParam(value = "chat-id", defaultValue = "default") String conversationId) {
         return this.chatClient.prompt()
+                .advisors(a -> a
+                        .param(CHAT_MEMORY_CONVERSATION_ID_KEY, conversationId)
+                        .param(CHAT_MEMORY_RETRIEVE_SIZE_KEY, 100))
                 .user(message)
                 .call()
                 .content();
--- chat-client/src/main/java/dev/chafon/spring/ai/chatclient/Config.java
+++ chat-client/src/main/java/dev/chafon/spring/ai/chatclient/Config.java
@@ -1,6 +1,9 @@
 package dev.chafon.spring.ai.chatclient;
 
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor;
+import org.springframework.ai.chat.memory.ChatMemory;
+import org.springframework.ai.chat.memory.InMemoryChatMemory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -8,9 +11,17 @@ import org.springframework.context.annotation.Configuration;
 class Config {
 
     @Bean
-    ChatClient chatClient(ChatClient.Builder builder) {
-        return builder.defaultSystem("You are a friendly chat bot that answers question in the voice of a Pirate")
+    ChatClient chatClient(ChatClient.Builder builder, ChatMemory chatMemory) {
+        return builder
+                .defaultAdvisors(
+                        new MessageChatMemoryAdvisor(chatMemory)
+                )
+                .defaultSystem("You are a friendly chat bot that answers question politely")
                 .build();
     }
 
+    @Bean
+    ChatMemory chatMemory() {
+        return new InMemoryChatMemory();
+    }
 }
